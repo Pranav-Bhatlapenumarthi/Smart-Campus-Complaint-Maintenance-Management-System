88ff793bce53ab3236e745477ae35a3b795925fd
ShubhamKumar0209 <sshubhamkr0229@gmail.com>
2025-12-28 00:22:15 +0530

    handle register and login of user

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,69 @@
+import { User } from "../models/user.model.js";
+import APIError from "../utils/APIError.js";
+import APIResponse from "../utils/APIResponse.js";
+import asyncHandler from "../utils/asyncHandler.js";
+
+// Register User
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password, role } = req.body;
+
+    if ([name, email, password].some((field) => field?.trim() === "")) {
+        throw new APIError(400, "All fields are required");
+    }
+
+    const existedUser = await User.findOne({ email });
+    if (existedUser) {
+        throw new APIError(409, "User with email already exists");
+    }
+
+    const user = await User.create({ name, email, password, role });
+    const createdUser = await User.findById(user._id).select("-password");
+
+    if (!createdUser) {
+        throw new APIError(500, "Something went wrong while registering the user");
+    }
+
+    return res.status(201).json(
+        new APIResponse(200, createdUser, "User registered successfully")
+    );
+});
+
+// Login User
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        throw new APIError(400, "email and password are required");
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+        throw new APIError(404, "User does not exist");
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+    if (!isPasswordValid) {
+        throw new APIError(401, "Invalid user credentials");
+    }
+
+    const accessToken = user.generateAccessToken();
+    const loggedInUser = await User.findById(user._id).select("-password");
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    };
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .json(
+            new APIResponse(
+                200, 
+                { user: loggedInUser, accessToken }, 
+                "User logged in successfully"
+            )
+        );
+});
+
+export { registerUser, loginUser };
